7ab8dd18f74fa2706f43fe09666a040b90b74c16
JayceDevelops <jayceiezzi@gmail.com>
2026-06-18 12:11:01 -0400

    User can now add tasks, Just need to create logic to display tasks

--- src/modules/rendermodal.js
+++ src/modules/rendermodal.js
@@ -7,6 +7,8 @@ import { InitSidebar } from "./init-sidebar";
 
 let selectedIcon = HomeIcon;
 
+let urgencyLevel = "noturgent";
+
 export const RenderModal = (type) => {
 
     const modalBackground = document.createElement("div");
@@ -21,16 +23,29 @@ export const RenderModal = (type) => {
     const body = document.querySelector('body');
 
     if (type === "task"){
+
+        const taskDiv = document.createElement('div');
+        taskDiv.classList.add('taskdiv');
+
         modalHeading.textContent = 'Add New Task';
-        modal.appendChild(modalHeading);
+        taskDiv.appendChild(modalHeading);
+
+        const taskName = document.createElement('h2');
+        taskName.textContent = "Task Name";
+        taskDiv.appendChild(taskName);
+
+        const taskNameInput = document.createElement('input');
+        taskNameInput.id = "taskName";
+        taskDiv.appendChild(taskNameInput);
 
         const listheading = document.createElement('h2');
-        listheading.textContent = "List";
-        modal.appendChild(listheading);
+        listheading.textContent = "Category";
+        taskDiv.appendChild(listheading);
 
         const categories = getListOfCategories();
         const dropdown = document.createElement('select');
-        modal.appendChild(dropdown);
+        dropdown.id = "categorydropdown";
+        taskDiv.appendChild(dropdown);
 
         categories.forEach((category) => {
             const option = document.createElement('option');
@@ -39,16 +54,62 @@ export const RenderModal = (type) => {
             dropdown.appendChild(option);
         });
 
-        modal.appendChild(dropdown);
+        const dueDateHeading = document.createElement('h2');
+        dueDateHeading.textContent = "Due Date";
+        taskDiv.appendChild(dueDateHeading);
 
+        const date = document.createElement('input');
+        date.type = 'datetime-local';
+        date.id = 'date';
+        taskDiv.appendChild(date);
+
+        const urgencyLevels = document.createElement('div');
+        urgencyLevels.classList.add('urgency');
+
+        const notUrgent = document.createElement('div');
+        notUrgent.classList.add('noturgent');
+        urgencyLevels.appendChild(notUrgent);
+
+        const slightUrgent = document.createElement('div');
+        slightUrgent.classList.add('slighturgent');
+        urgencyLevels.appendChild(slightUrgent);
+
+        const urgent = document.createElement('div');
+        urgent.classList.add('urgent');
+        urgencyLevels.appendChild(urgent);
+
+        taskDiv.appendChild(urgencyLevels);
+
+        const taskButtons = document.createElement('div');
+        taskButtons.classList.add('taskbuttons');
+
+        const cancelTask = document.createElement('button');
+        cancelTask.classList.add('canceltask');
+        cancelTask.textContent = "Cancel";
+        taskButtons.appendChild(cancelTask);
+
+        const addTask = document.createElement('button');
+        addTask.classList.add('addtask');
+        addTask.textContent = "Add Task";
+        taskButtons.appendChild(addTask);
+
+        taskDiv.appendChild(taskButtons);
+        modal.appendChild(taskDiv);
+
+        createCancelEvent(modalBackground, cancelTask);
+        createAddEvent(modalBackground, modal, addTask, "task");
+        
         modalBackground.appendChild(modal);
         body.insertBefore(modalBackground, container);
     }
     else {
 
+        const categoryDiv = document.createElement('div');
+        categoryDiv.classList.add('categorydiv');
+
         // Heading
         modalHeading.textContent = 'Create New Category';
-        modal.appendChild(modalHeading);
+        categoryDiv.appendChild(modalHeading);
 
         // Input Heading and Text Box
         const inputDiv = document.createElement('div');
@@ -62,7 +123,7 @@ export const RenderModal = (type) => {
         input.id = 'category';
         inputDiv.appendChild(input);
 
-        modal.appendChild(inputDiv);
+        categoryDiv.appendChild(inputDiv);
 
         // Icon Chooser
         const iconDiv = document.createElement('div');
@@ -113,7 +174,7 @@ export const RenderModal = (type) => {
         });
         
 
-        modal.appendChild(iconDiv);
+        categoryDiv.appendChild(iconDiv);
         
         // Cancel and Add Button
         const buttonDiv = document.createElement('div');
@@ -129,10 +190,11 @@ export const RenderModal = (type) => {
         addButton.textContent = 'Add Category';
         buttonDiv.appendChild(addButton);
 
-        modal.appendChild(buttonDiv);
+        categoryDiv.appendChild(buttonDiv);
+        modal.appendChild(categoryDiv);
 
         createCancelEvent(modalBackground, cancelButton);
-        createAddEvent(modalBackground, modal, addButton);
+        createAddEvent(modalBackground, modal, addButton, "category");
 
         modalBackground.appendChild(modal);
         body.insertBefore(modalBackground, container);
@@ -157,34 +219,64 @@ const createCancelEvent = (container, cancel) => {
     });
 };
 
-const createAddEvent = (container, modal, add) => {
+const createAddEvent = (container, modal, add, type) => {
 
-    add.addEventListener("click", () => {
+    if (type === "category"){
+        add.addEventListener("click", () => {
         
-        
-        let CustomCategories = localStorage.getItem('Custom');
-        CustomCategories = JSON.parse(CustomCategories);
+            let CustomCategories = localStorage.getItem('Custom');
+            CustomCategories = JSON.parse(CustomCategories);
 
-        const input = document.querySelector('#category').value;
-        console.log(input);
-        const category = {
-            id: crypto.randomUUID(),
-            name: input,
-            image: selectedIcon
-        };
+            const input = document.querySelector('#category').value;
+            console.log(input);
+            const category = {
+                id: crypto.randomUUID(),
+                name: input,
+                image: selectedIcon
+            };
 
-        CustomCategories.push(category);
+            CustomCategories.push(category);
 
-        
-        CustomCategories = JSON.stringify(CustomCategories);
-        localStorage.setItem('Custom', CustomCategories);
+            
+            CustomCategories = JSON.stringify(CustomCategories);
+            localStorage.setItem('Custom', CustomCategories);
 
-        focusBackground(container);
+            focusBackground(container);
 
-        InitSidebar();
+            InitSidebar();
 
-        modal.remove();
-    });
+            modal.remove()
+        });
+    }
+    else {
+        add.addEventListener("click", () => {
+            let tasks = localStorage.getItem('Tasks');
+            tasks = JSON.parse(tasks);
+
+            const taskName = document.querySelector("#taskName").value;
+
+            const dropdown = document.querySelector('#categorydropdown');
+            const dropdownvalue = dropdown.options[dropdown.selectedIndex].text;
+
+            const duedate = document.querySelector('#date').value;
+
+            const task = {
+                id: crypto.randomUUID(),
+                name: taskName,
+                category: dropdownvalue,
+                duedate: duedate,
+                urgency: urgencyLevel
+            }
+
+            tasks.push(task);
+
+            tasks = JSON.stringify(tasks);
+            localStorage.setItem('Tasks', tasks);
+
+            focusBackground(container);
+            modal.remove();
+        });
+    };
 };
 
 const updateImageSelectedBorder = () => {
